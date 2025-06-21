b209177cb293968ced0560c5e23a4e13f70ce4d9
yangjiashan <526056821@qq.com>
2025-06-21 23:39:08 +0800

    增加配置

--- src/main/java/com/zoe/java/ai/langchain4j/config/ZoeAgentConfig.java
+++ src/main/java/com/zoe/java/ai/langchain4j/config/ZoeAgentConfig.java
@@ -67,27 +67,28 @@ public class ZoeAgentConfig {
 
     }
 
-//    @Autowired
-//    private EmbeddingStore embeddingStore;
+
+    @Autowired
+    private EmbeddingStore embeddingStore;
 
 //    @Autowired
 //    private EmbeddingModel embeddingModel;
-//
-//    @Bean
-//    ContentRetriever contentRetrieverZoePincone() {
-//
-//        // 创建一个 EmbeddingStoreContentRetriever 对象，用于从嵌入存储中检索内容
-//        return EmbeddingStoreContentRetriever
-//                .builder()
-//                // 设置用于生成嵌入向量的嵌入模型
+
+    @Bean
+    ContentRetriever contentRetrieverZoePincone() {
+
+        // 创建一个 EmbeddingStoreContentRetriever 对象，用于从嵌入存储中检索内容
+        return EmbeddingStoreContentRetriever
+                .builder()
+                // 设置用于生成嵌入向量的嵌入模型
 //                .embeddingModel(embeddingModel)
-//                // 指定要使用的嵌入存储
-//                .embeddingStore(embeddingStore)
-//                // 设置最大检索结果数量，这里表示最多返回 1 条匹配结果
-//                .maxResults(1)
-//                // 设置最小得分阈值，只有得分大于等于 0.8 的结果才会被返回
-//                .minScore(0.8)
-//                // 构建最终的 EmbeddingStoreContentRetriever 实例
-//                .build();
-//    }
+                // 指定要使用的嵌入存储
+                .embeddingStore(embeddingStore)
+                // 设置最大检索结果数量，这里表示最多返回 1 条匹配结果
+                .maxResults(1)
+                // 设置最小得分阈值，只有得分大于等于 0.8 的结果才会被返回
+                .minScore(0.8)
+                // 构建最终的 EmbeddingStoreContentRetriever 实例
+                .build();
+    }
 }
