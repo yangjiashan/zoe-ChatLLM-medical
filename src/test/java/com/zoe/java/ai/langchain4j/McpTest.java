a50a1ec18a5f5ecd3369c0562e35a1dd4679ae0e
yjiashan <yjiashan@linewell.com>
2025-07-10 15:07:35 +0800

    增加mcp测试代码

--- src/test/java/com/zoe/java/ai/langchain4j/McpTest.java
+++ src/test/java/com/zoe/java/ai/langchain4j/McpTest.java
@@ -41,7 +41,7 @@ public class McpTest {
 //        McpTransport transport = new StdioMcpTransport.Builder()
 ////                .command(List.of("/usr/local/bin/docker", "run", "-e", "GITHUB_PERSONAL_ACCESS_TOKEN", "-i", "mcp/github"))
 //                .command(List.of("npx.cmd", "-y", "@modelcontextprotocol/server-github"))
-////                .environment(Map.of("GITHUB_PERSONAL_ACCESS_TOKEN", "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"))
+////                .environment(Map.of("GITHUB_PERSONAL_ACCESS_TOKEN", ""))
 //                .logEvents(true)
 //                .build();
 //
